32402025aa37b9c9e37127495b1faebd55b659cb
ViniciusDaCunha <99222396+ViniciusDaCunha@users.noreply.github.com>
2022-03-28 11:59:37 -0300

    Update aplication.java

--- src/teste/aplication.java
+++ src/teste/aplication.java
@@ -2,23 +2,23 @@ package teste;
 
 import java.util.Scanner;
 
-public class aplication {
+public class application {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		int x;
-		System.out.println("Digite um numero: ");
+		System.out.println("Digite um n√∫mero: ");
 		x = sc.nextInt();
 
-		System.out.println("S„o pares: ");
+		System.out.println("S√£o pares: ");
 		for (int i = 0; i <= x; i++) {
 			if (i % 2 == 0) {
 				System.out.println((x - i));
 			}
 		}
 		
-		System.out.println("S„o impares: ");
+		System.out.println("S√£o impares: ");
 		for (int j = 0; j <= x; j++) {
 			if (j % 2 == 1) {
 				System.out.println((x - j));
